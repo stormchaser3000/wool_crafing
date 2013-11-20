12206bf9b513641b89f2dc76e168c405f639eae4
why would you want to know this <stormchaser300060@yahoo.com>
2013-11-19 22:48:01 -0600

    Update init.lua
    just a few extra crafting recepies

--- init.lua
+++ init.lua
@@ -1,8 +1,21 @@
-
 minetest.register_craft({
 	output = 'wool:white',
 	recipe = {
 		{'default:grass_1', 'default:grass_1'},
 		{'default:grass_1', 'default:grass_1'},
+	}
+})
+
+minetest.register_craft({
+	output = 'default:steel_block',
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot'},
+	}
+})
+
+minetest.register_craft({
+	output = 'default:brick_block',
+	recipe = {
+		{'default:clay_brick', 'default:clay_brick'},
 	}
 })
